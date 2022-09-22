f82dcec6e43ddf0a4e7f822ad3195dd448479fbd
Abel <shawelabel9@gmail.com>
2022-09-22 12:27:23 +0300

    medium level challenge completed

--- dark-mode/src/routes/App/components/App.js
+++ dark-mode/src/routes/App/components/App.js
@@ -1,19 +1,20 @@
-import React from 'react';
+import React,{useState} from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faMoon } from '@fortawesome/free-solid-svg-icons';
+import { faMoon,faSun } from '@fortawesome/free-solid-svg-icons';
 import '../styles/_app.scss';
 
 function App() {
+  const [darkMode,setDarkMode] = useState(false)
   return (
-    <div className="app">
+    <div className={darkMode?"dark-mode": "app"}>
       <div className="level">
         <div>
           <h1 className="title">Dark Mode Challenge</h1>
         </div>
 
         {/* --The button that should toggle dark mode-- */}
-        <button className="app__dark-mode-btn icon level-right">
-          <FontAwesomeIcon icon={faMoon} />
+        <button className="app__dark-mode-btn icon level-right" onClick={() => setDarkMode(prev => !prev)}>
+          <FontAwesomeIcon icon={darkMode?faSun:faMoon} style={darkMode? {color:'#FFA500'}: null}/>
         </button>
 
       </div>

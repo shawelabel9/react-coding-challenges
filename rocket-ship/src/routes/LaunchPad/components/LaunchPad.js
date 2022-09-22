b4a01f2d25018a12b07e0cfe715883776f108d76
Abel <shawelabel9@gmail.com>
2022-09-22 11:53:25 +0300

    rocket ship challenge solved

--- rocket-ship/src/routes/LaunchPad/components/LaunchPad.js
+++ rocket-ship/src/routes/LaunchPad/components/LaunchPad.js
@@ -6,7 +6,7 @@ export default function LaunchPad() {
   const [, triggerRerender] = useState(Date.now());
   
   useEffect(() => {
-    setInterval(() => { triggerRerender(Date.now()); }, 500);
+    // setInterval(() => { triggerRerender(Date.now()); }, 500);
   }, [])
   
   return (
